af9acaa1c496dd96257d8a5d27512045a14677d1
Nakrani <ashish.nakrani.60@gmail.com>
2020-12-01 23:38:35 +0530

    Refactor: add new loader and update UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -160,7 +160,7 @@ var App = function() {
                         pos += 10;
                     }
                     $('.menu-indicator').css({opacity:1, top: pos, height: height, width: width})
-                }, 180)
+                }, 200)
                 // $('.menu-indicator').css({opacity:1, top: pos, height: height, width: width})
 
             });
@@ -168,7 +168,7 @@ var App = function() {
                 setTimeout(function () {
 
                     $('.menu-indicator').css({opacity: 0})
-                }, 180)
+                }, 200)
             });
             menuWrapper.on('scroll', function () {
                 $('.menu-indicator').css({opacity: 0})
