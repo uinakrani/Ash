552270a71493eef541d2a110f9962e38f261f4ee
Nakrani <ashish.nakrani.60@gmail.com>
2020-12-02 20:49:32 +0530

    Refactor: add new loader and update UI

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,4 +1,9 @@
 window.addEventListener("load", function(){
     var load_screen = document.getElementById("load_screen");
-    document.body.removeChild(load_screen);
+    setTimeout(function () {
+        $(load_screen).css({'transform':'scale(1.1)', 'transition': '1s', 'opacity': '0'});
+        setTimeout(function () {
+            document.body.removeChild(load_screen);
+        }, 500)
+    }, 1500)
 });
